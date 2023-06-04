0625af728caec9a99e0a00491624ac533d918dcc
Koshun-code <sit.com.k@gmail.com>
2023-06-04 22:32:49 +0300

    simple state

---
+++ script.js
@@ -0,0 +1,20 @@
+const app = () => {
+  let counterValue = 0;
+
+  const result = document.getElementById('result');
+  const incHandler = () => {
+    counterValue += 1;
+    result.textContent = counterValue;
+  }
+  const decHandler = () => {
+    counterValue -= 1;
+    result.textContent = counterValue;
+  }
+
+  const inc = document.getElementById('increment');
+  inc.addEventListener('click', incHandler);
+  const dec = document.getElementById('decrement');
+  dec.addEventListener('click', decHandler);
+};
+
+app();
